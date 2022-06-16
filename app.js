5e01e9ce46e667bf1dc4b318a369cdeac6f54924
bopcho <50643135+bopcho@users.noreply.github.com>
2022-06-16 23:02:59 +0200

    Rock Paper Scissors

---
+++ app.js
@@ -0,0 +1,71 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+const yourScoreSpan = document.querySelector('[data-your-score]');
+const description = document.getElementById('description');
+const SELECTIONS = [
+    {
+        name: 'Lapis',
+        emoji: '🪨',
+        beats: 'Scalpellus'
+    },
+    {
+        name: 'Papyrus',
+        emoji: '📜',
+        beats: 'Lapis'
+    },
+    {
+        name: 'Scalpellus',
+        emoji: '✂️',
+        beats: 'Papyrus'
+    }
+];
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        const selection = SELECTIONS.find(selection => selection.name === selectionName);
+        makeSelection(selection);
+    })
+});
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const copmuterWinner = isWinner(computerSelection, selection);
+
+    addSelectionResult(computerSelection, copmuterWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) {
+        incrementScore(yourScoreSpan);
+        description.innerText = `You won! You chose ${selection.emoji} and the computer chose ${computerSelection.emoji}`;
+    } else if (copmuterWinner) {
+        incrementScore(computerScoreSpan);
+        description.innerText = `You lost! You chose ${selection.emoji} and the computer chose ${computerSelection.emoji}`;
+    } else {
+        description.innerText = `It's a tie! You chose ${selection.emoji} and the computer chose ${computerSelection.emoji}`;
+    }
+
+};
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div');
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name;
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex];
+}
